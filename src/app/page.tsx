f3dec8a9beba9e7097cf35b58086447a28938a2c
MarcialFantini <marcialfantini@gmail.com>
2023-10-05 09:25:36 -0300

    update avance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroHome from "@/components/HeroHome";
 import styles from "./page.module.css";
+import BarHomeCompany from "@/components/BarHomeCompany";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <HeroHome></HeroHome>
+      <BarHomeCompany></BarHomeCompany>
     </main>
   );
 }
---
+++ src/components/BarHomeCompany/index.tsx
@@ -0,0 +1,25 @@
+import style from "./style.module.css";
+
+function BarHomeCompany() {
+  return (
+    <section className={style.section}>
+      <div className={style.containerAll}>
+        <div>
+          <h2 className={style.title}>
+            Trusted by 200+ companies around the world
+          </h2>
+        </div>
+        <div>
+          <p className={style.text}>
+            Vulputate molestie molestie amet leo blandit accumsan. Sapien sed
+            amet tellus purus sit odio eget. Diam morbi faucibus vitae neque id
+            in. Nullam sed et dapibus nunc, porta enim orci urna, sit. Lectus
+            ac.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default BarHomeCompany;
